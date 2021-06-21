51a1b567cf65d35ccf5d0095c7c7b79e99f62f11
GitHub Action <action@github.com>
2021-06-21 19:30:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/mrmaniacc/marxmanager/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mrmaniacc/marxmanager/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mrmaniacc.marxmanager.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mrmaniacc.marxmanager.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/mrmaniacc/marxmanager/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mrmaniacc/marxmanager/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mrmaniacc.marxmanager.services
 
+import com.github.mrmaniacc.marxmanager.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
